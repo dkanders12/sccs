8f21c66c973d9a8089d951729da1821765f97e42
dkanders12 <dkanders12@gmail.com>
2023-11-16 13:56:40 +0100

    t

--- js/script.js
+++ js/script.js
@@ -11,3 +11,32 @@ document.getElementById("contact").addEventListener("click", () => {
   var kontactContainer = document.querySelector(".kontact-container");
   kontactContainer.classList.toggle("active");
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  let form = document.getElementById("myForm");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let firstname = form.firstname;
+    let lastname = form.lastname;
+    let errorBox = document.getElementById("errorBox");
+
+    // Clear previous error messages
+    errorBox.innerHTML = "";
+
+    if (firstname.value.trim() === "") {
+      errorBox.classList.add("show");
+      errorBox.innerHTML += "Need A Name,  ";
+      return false;
+    }
+    if (lastname.value.trim() === "") {
+      errorBox.classList.add("show");
+      errorBox.innerHTML += "Need A Lastname,  ";
+      return false;
+    }
+
+    // If both fields are filled, you can reload the page
+    location.reload();
+  });
+});
